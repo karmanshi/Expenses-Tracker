14e323a0bad29c75f24ec3b0906cf8ca8308cad7
Karmanshi Shrivastava <80811897+karmanshi@users.noreply.github.com>
2024-08-24 18:32:41 +0000

    add react router

--- expenses-tracker/src/App.js
+++ expenses-tracker/src/App.js
@@ -1,10 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import {  Routes, Route } from "react-router-dom";
 import Expanses from './Component/Expanses';
+import Profile from './Component/Profile';
 
 function App() {
   return (
- <Expanses/>
+    <>
+    <Routes>
+        <Route path="/" element={<Expanses />}/>
+        <Route path="/profile/" element={<Profile/>}/>
+         
+         
+        
+      </Routes>
+    </>
   );
 }
 
--- expenses-tracker/src/Component/navbar.jsx
+++ expenses-tracker/src/Component/navbar.jsx
@@ -1,27 +1,51 @@
 import React from 'react'
+import { Outlet, Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <nav className="bg-black  border-gray-200 dark:bg-gray-900 mb-6 ">
-    <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-      <a
-        href="https://flowbite.com/"
-        className="flex items-center space-x-3 rtl:space-x-reverse"
-      >
-        <img
-          src="https://flowbite.com/docs/images/logo.svg"
-          className="h-8"
-          alt="Flowbite Logo"
-        />
-        <span className="self-center text-white text-2xl font-semibold whitespace-nowrap dark:text-white">
-         Expenses Tracker
-        </span>
-      </a>
-      
-     
-    </div>
-  </nav>
-  
+    <>
+      <nav className="bg-black  border-gray-200 dark:bg-gray-900 mb-6 ">
+        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+          <a
+            href="https://flowbite.com/"
+            className="flex items-center space-x-3 rtl:space-x-reverse"
+          >
+            <img
+              src="https://flowbite.com/docs/images/logo.svg"
+              className="h-8"
+              alt="Flowbite Logo"
+            />
+            <span className="self-center text-white text-2xl font-semibold whitespace-nowrap dark:text-white">
+              Expenses Tracker
+            </span>
+          </a>
+          <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+            <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg  md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 ">
+
+              <li>
+                <Link
+                  to="/"
+                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500  dark:hover:text-white md:dark:hover text-gray-300 text-xl"
+                >
+                  Dashboard
+                </Link>
+              </li>
+
+              <li>
+                <Link
+                  to="/profile/"
+                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500  dark:hover:text-white md:dark:hover text-gray-300 text-xl"
+                >
+                 Profile
+                </Link>
+              </li>
+            </ul>
+          </div>   
+        </div>
+      </nav>
+      <Outlet />
+    </>
+
   )
 }
 
